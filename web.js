37f5d2d399d08c5992e8abeff324d393ef3789c1
Stacy Pennington <truk@mazzy.com>
2013-07-07 21:52:23 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,11 +6,12 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   /*response.send('Hello World 2!'); */
   fs.readFileSync('./index.html', function(err, data){
+    var buffer = data;
     response.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
     if (err) {
       throw err;
     }
-    response.write(buffer.toString(data));
+    response.write(buffer.toString('utf8');
     response.end();
   });
 });
